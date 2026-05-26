941e3f7518e20b91a1d7c5cf11a26a3e70d392f9
heycker lima <heycker.paula@edu.senai.br>
2026-05-26 02:26:37 +0000

    Atividade 5: Tarefa 1.5: Para que o Dart reconheça esta classe, importei o arquivo que a define. Adicionando a importação ao início do arquivo command_runner/lib/src/arguments.dart

--- arguments.dart
+++ arguments.dart
@@ -1,12 +1,11 @@
+import '../command_runner.dart';
+
 enum OptionType { flag, option }
-// Paste this new class below the enum you added
+
 abstract class Argument {
   String get name;
   String? get help;
 
-  // In the case of flags, the default value is a bool.
-  // In other options and commands, the default value is a String.
-  // NB: flags are just Option objects that don't take arguments
   Object? get defaultValue;
   String? get valueHelp;
 
