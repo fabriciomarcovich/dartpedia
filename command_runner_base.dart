eda0ce535ba33c5dc701a257291f4ad431215e2a
giovani courel <giovani.courel@edu.senai.br>
2026-06-10 02:06:01 +0000

    Atividade 8: Tarefa 2/2: Atualizei o run método para usar o onOutput argumento

--- command_runner_base.dart
+++ command_runner_base.dart
@@ -47,7 +47,7 @@ class CommandRunner {
 Versao: 0.0.2
 
  Codigo:
-*/
+
 import 'dart:collection';
 import 'dart:io';
 import 'arguments.dart';
@@ -69,6 +69,67 @@ class CommandRunner {
     if (results.command != null) {
       Object? output = await results.command!.run(results);
       print(output.toString());
+    }
+  }
+
+  void addCommand(Command command) {
+    // TODO: handle error (Commands can't have names that conflict)
+    _commands[command.name] = command;
+    command.runner = this;
+  }
+
+  ArgResults parse(List<String> input) {
+    var results = ArgResults();
+    results.command = _commands[input.first];
+    return results;
+  }
+
+  // Returns usage for the executable only.
+  String get usage {
+    final exeFile = Platform.script.path.split('/').last;
+    return 'Usage: dart bin/$exeFile <command> [commandArgs] [...options]';
+  }
+}
+
+-------------------------------------------------------------------------------
+
+Versao: 0.0.3
+
+ Codigo:
+*/
+import 'dart:collection';
+import 'dart:io';
+import 'arguments.dart';
+
+class CommandRunner {
+  CommandRunner({this.onOutput, this.onError});
+
+  // If not null, this method is used to handle output.
+  FutureOr<void> Function(String)? onOutput;
+  FutureOr<void> Function(Object)? onError;
+
+  final Map<String, Command> _commands = <String, Command>{};
+
+  UnmodifiableSetView<Command> get commands =>
+      UnmodifiableSetView(<Command>{..._commands.values});
+
+  Future<void> run(List<String> input) async {
+    try {
+      final ArgResults results = parse(input);
+      if (results.command != null) {
+        Object? output = await results.command!.run(results);
+        if (onOutput != null) {
+          await onOutput!(output.toString());
+        } else {
+          print(output.toString());
+        }
+      }
+    } on Exception catch (exception) {
+      if (onError != null) {
+        onError!(exception);
+      } else {
+        rethrow;
+      }
     }
   }
 
