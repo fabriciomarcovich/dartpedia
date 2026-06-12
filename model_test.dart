518604dff5c2001d1e8b1060a2d5a27dea625ba2
heycker lima <heycker.paula@edu.senai.br>
2026-06-12 19:01:43 +0000

    Ativdade 10: Tarefa 4.4: Criei um teste para o SearchResultsmodelo

--- model_test.dart
+++ model_test.dart
@@ -33,3 +33,14 @@ void main() {
   });
 }
 
+    test('deserialize Open Search results example data from json file '
+        'into an SearchResults object', () async {
+      final String resultsString =
+          await File(openSearchResponse).readAsString();
+      final List<Object?> resultsAsList =
+          jsonDecode(resultsString) as List<Object?>;
+      final SearchResults results = SearchResults.fromJson(resultsAsList);
+      expect(results.results.length, greaterThan(1));
+    });
+  });
+}
