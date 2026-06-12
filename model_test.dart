f5a3be412a1b5e1facca46f5e9d0cae67a71da9d
heycker lima <heycker.paula@edu.senai.br>
2026-06-12 18:54:51 +0000

    Atividade 10: Tarefa 4.1: Utilizei a groupfunção para agrupar testes relacionados.

--- model_test.dart
+++ model_test.dart
@@ -10,3 +10,9 @@ const String dartLangSummaryJson = './test/test_data/dart_lang_summary.json';
 const String catExtractJson = './test/test_data/cat_extract.json';
 const String openSearchResponse = './test/test_data/open_search_response.json';
 
+void main() {
+  group('deserialize example JSON responses from wikipedia API', () {
+    // Tests will go here
+  });
+}
+
