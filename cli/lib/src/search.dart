3c2b691a8649c1fa7ad9e1dc32f292e8f5070ad3
Fabricio Marcovich <fabricio.m.reis@edu.senai.br>
2026-06-14 03:15:34 +0000

    Atividade 12: Tarefa 4.3: Adicionei a logica prinicipal ao run metodo

--- cli/lib/src/search.dart
+++ cli/lib/src/search.dart
@@ -2,7 +2,7 @@
 Versao: 0.0.0
 
 Codigo:
-*/
+
 import 'dart:async';
 import 'dart:io';
 
@@ -35,5 +35,54 @@ class SearchCommand extends Command {
   FutureOr<String> run(ArgResults args) async {
     // The rest of the function will be added below.
     // ...
+  }
+}
+-------------------------------------------------------------------------------
+Versao: 0.0.1
+
+codigo:
+*/
+import 'dart:async';
+import 'dart:io';
+
+import 'package:command_runner/command_runner.dart';
+import 'package:logging/logging.dart';
+import 'package:wikipedia/wikipedia.dart';
+
+class SearchCommand extends Command {
+  SearchCommand({required this.logger});
+
+  final Logger logger;
+
+  @override
+  String get description => 'Search for Wikipedia articles.';
+
+  @override
+  bool get requiresArgument => true;
+
+  @override
+  String get name => 'search';
+
+  @override
+  String get valueHelp => 'STRING';
+
+  @override
+  String get help =>
+      'Prints a list of links to Wikipedia articles that match the given term.';
+
+  @override
+  FutureOr<String> run(ArgResults args) async {
+    if (requiresArgument &&
+        (args.commandArg == null || args.commandArg!.isEmpty)) {
+      return 'Please include a search term';
+    }
+
+    final buffer = StringBuffer('Search results:');
+    final SearchResults results = await search(args.commandArg!);
+
+    for (var result in results.results) {
+      buffer.writeln('${result.title} - ${result.url}');
+    }
+    return buffer.toString();
   }
 }
